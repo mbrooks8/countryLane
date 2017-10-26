a8409300ff9d302004f1314d30ca2dcbaff306ab
Afropenguin <mbrooks8@ucmerced.edu>
2017-10-26 16:34:26 -0700

    Updated style

--- assets/template/home/topNav.php
+++ assets/template/home/topNav.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Hestia Property Management Software</title>
+		<title>Country Lane Appartments</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
 		<link rel="stylesheet" type="text/css"  href="./assets/css/hestia.css">
-		<meta name="description" content="Property Management Software geared towards giving you the ability to efficiently manage your proporties.">
+		<meta name="description" content="46710 Crawford St. Fremont California 94539">
 		<link rel="icon" href="../assets/img/logo.png">
-		<meta name="author" content="Michael Brooks, Esteban Perez">
+		<meta name="author" content="Michael Brooks">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
 	</head>
 	<body>
 
 
 		<!--Side Navigation Bar-->
-		<div class="sidebar " id="side">
+<!--		<div class="sidebar " id="side">
 			<div class="sideHeader" style="text-align:center;">
 				<div class="sideHeader" style="text-align:center;">
 					<h1 class="white-text"><a href="./index" class="logo white-text">Hestia</a></h1>
@@ -33,9 +33,9 @@
 			</div>
 		</div>
 
-		<div class="site-content" style="" >
+		<div class="site-content" style="background-color:transparent;" >-->
 			<!--Top Navigation Bar-->
-			<nav class="">
+<!--			<nav class="">
 				<div class="nav-wrapper">
 					<a href="./index" class="logo"><img src="./assets/img/logo.svg" style="height:100%;"></a>
 					<ul class="left hide-on-med-and-down">
@@ -48,7 +48,7 @@
 					</ul>
 					<ul class="right hide-on-med-and-down">
 						<li><a href="./login">Log In</a></li>
-						<!--this should be a modal or it could be a page idk-->
+						this should be a modal or it could be a page idk
 						<li><a href="./demo" class="button raised border">Request a Demo</a></li>
 					</ul>
 					<ul class="right hide-on-large-only">
@@ -57,4 +57,4 @@
 					</ul>
 				</div>
 			</nav>
-		</div>
+		</div>-->
--- index.php
+++ index.php
@@ -3,16 +3,25 @@ include("./assets/template/home/topNav.php");
 ?>
 
 <div class="banner large">
-	<div class="content">
-		<div class="container" style="position:absolute; background-color:#CC4F55; width:100vw; height:30vh; bottom:0;">
-			<h1>Do Something With This Later</h1>
-			<h1>Country Lane Appartments</h1>
-			<p>46710 Crawford Street <br> Fremont, California 94539</p>
+	<div class="content" >
+		<div class="container" style="position:absolute; background-color:#CC4F55; width:100vw; bottom:0; padding-bottom:2%; padding-top:2%;padding-left:5%;padding-right:5%;">
+			<h1 class="white-text">Country Lane Appartments</h1>
+			<h5 class="white-text">46710 Crawford Street &#xb7; Fremont, California 94539</h5>
 		</div>
 	</div>
-	<div class="imgDiv" style="background: url(./assets/img/red160-apartments-exterior.jpg)"></div>
+	<div class="imgDiv" style="background: url(./assets/img/test.jpg)"></div>
 </div>
+<section class="large">
+	<div class="row">
+		<div class="col s12">
+			<div style="width:95%; margin-left:2.5%; height:60vh; overflow:hidden;">
+				<img style="width:100vw;" src="./assets/img/test2.jpg">
 
+			</div>
+
+		</div>
+	</div>
+</section>
 <?php
 include("./assets/template/home/botNav.php");
 ?>
