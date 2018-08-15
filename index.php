9bf8d35ba2569bf8910f7028711109cb8e6a3f21
Afropenguin <mbrooks8@ucmerced.edu>
2018-08-15 10:10:51 -0700

    Had issues with pushing from last folder. Created new folder

--- index.php
+++ index.php
@@ -3,27 +3,27 @@ include("./assets/template/home/topNav.php");
 ?>
 
 <div class="banner large">
-<!--			<nav style="background-color:rgba(0,0,0,.1);">
-				<div class="nav-wrapper">
-					<a href="#welcome" class="logo">Logo</a>
-					<ul class="right hide-on-med-and-down">
-						<li><a href="#link1" class="black-text">Link1</a></li>
-						<li><a href="#link2" class="black-text">Link2</a></li>
-						<li><a href="#link3" class="black-text">Link3</a></li>
-						<li><a href="#link4" class="black-text">Link4</a></li>
-					</ul>
-					<ul class="right hide-on-large-only">
-						<li><a class="sideToggle"><i class="fa fa-bars" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-			</nav>-->
+	<!--			<nav style="background-color:rgba(0,0,0,.1);">
+<div class="nav-wrapper">oala
+<a href="#welcome" class="logo">Logo</a>
+<ul class="right hide-on-med-and-down">
+<li><a href="#link1" class="black-text">Link1</a></li>
+<li><a href="#link2" class="black-text">Link2</a></li>
+<li><a href="#link3" class="black-text">Link3</a></li>
+<li><a href="#link4" class="black-text">Link4</a></li>
+</ul>
+<ul class="right hide-on-large-only">
+<li><a class="sideToggle"><i <cla></cla>ss="fa fa-bars" aria-hidden="true"></i></a></li>
+</ul>
+</div>
+</nav>-->
 	<div class="content" >
 		<div class="container" style="position:absolute; background-color:#CC4F55; width:100vw; bottom:0; padding-bottom:2%; padding-top:2%;padding-left:5%;padding-right:5%;">
-			<h1 class="white-text">Country Lane Apartments</h1>
+			<h1 class="white-text">Country Lane Apartment</h1>
 			<h5 class="white-text">46710 Crawford Street &#xb7; Fremont, California 94539</h5>
 			<h5 class="white-text">(510) 579-5088</h5>
 		</div>
-		<div class="imgDiv" style="background: url(./assets/img/test2.jpg)"></div>
+		<div class="imgDiv" style="background: url(./assets/img/MVIMG_20171107_162821.jpg)"></div>
 	</div>
 
 </div>
@@ -35,7 +35,7 @@ include("./assets/template/home/topNav.php");
 					<img style="width:100vw;" src="./assets/img/test2.jpg">
 				</div>
 				<div class="hide-on-small-only">
-				    <br><br><br>
+					<br><br><br>
 				</div>
 				<div class="row">
 
@@ -52,50 +52,39 @@ include("./assets/template/home/topNav.php");
 	</div>
 </section>
 <hr class="long">
-
-<!--SECOND THING -->
 <section>
 	<div class="row">
 		<div class="container large">
 			<div class="col l6 s12">
 				<div class="row">
 					<div class="col s12">
-						<h1 class="black-text" style="text-align: right;">The Space</h1>
+
+						<h1 class="black-text" style="text-align: right;">The Single</h1>
 						<br>
 
-						<div class="hide-on-large-only" style="min-height:20vh;background: url(./assets/img/test2.jpg); right:0;"></div>
+						<div class="hide-on-large-only" style="min-height:20vh;background: url(./assets/img/MVIMG_20171107_162943.jpg); right:0; background-position: center; background-size: 100%; "></div>
 
 
 						<br>
-						<h3 class="black-text" style="text-align: right;">Accomodates: <span><b>4</b></span></h3>
-						<h3 class="black-text" style="text-align: right;">Bedrooms: <span><b>4</b></span></h3>
-						<h3 class="black-text" style="text-align: right;">Bathrooms: <span><b>4</b></span></h3>
-						<h3 class="black-text" style="text-align: right;">Property Type: <span><b>Townhouse</b></span></h3>
-						<h3 class="black-text" style="text-align: right;">Room Type: <span><b>Entire Apartment</b></span></h3>
-						<h3 class="black-text" style="text-align: right;">Price: <span><b>$2400/mo</b></span></h3>
-						<h3 class="black-text" style="text-align: right;">Availibility: <span><b>December</b></span></h3>
+						<h3 class="black-text" style="text-align: right;">Bedrooms: <span><b>1</b></span></h3>
+						<h3 class="black-text" style="text-align: right;">Bathrooms: <span><b>1</b></span></h3>
+						<h3 class="black-text" style="text-align: right;">Price: <span><b>$1895/mo</b></span></h3>
 					</div>
 				</div>
 				<br>
 				<br>
 				<div class="col s12 hide-on-med-and-down" style="border:1px solid black;">
 					<div class="row">
-						<div class="col s4 valign">
-							<div>
-								<h1 class="black-text" style="text-align:center;">4</h1>
-								<h3 class="black-text">Guests</h3>
-							</div>
-						</div>
-						<div class="col s4 valign">
-							<div>
-								<h1 class="black-text" style="text-align:center;">2</h1>
-								<h3 class="black-text">Bedrooms</h3>
-							</div>
-						</div>
-						<div class="col s4 valign">
+						<div class="col s6 valign">
 							<div>
 								<h1 class="black-text" style="text-align:center;">1</h1>
-								<h3 class="black-text">Bathroom</h3>
+								<h3 class="black-text">Bed</h3>
+							</div>
+						</div>
+						<div class="col s6 valign">
+							<div>
+								<h1 class="black-text" style="text-align:center;">1</h1>
+								<h3 class="black-text">Bath</h3>
 							</div>
 						</div>
 
@@ -104,7 +93,104 @@ include("./assets/template/home/topNav.php");
 			</div>
 			<div class="col l1 hide-on-med-and-down"></div>
 			<div class="col l5 s12 hide-on-med-and-down">
-				<div style="min-height:90vh;background: url(./assets/img/test2.jpg); right:0;"></div>
+				<div style="min-height:90vh;background: url(./assets/img/MVIMG_20171107_162943.jpg); background-position: center; background-size: auto 100%; right:0;"></div>
+			</div>
+		</div>
+	</div>
+</section>
+<hr class="long">
+<section>
+	<div class="row">
+		<div class="container large">
+
+			<div class="col l5 s12 hide-on-med-and-down">
+				<div style="min-height:90vh;background: url(./assets/img/MVIMG_20171107_162943.jpg); background-position: center; background-size: auto 100%; right:0;"></div>
+			</div>
+			<div class="col l1 hide-on-med-and-down"></div>
+			<div class="col l6 s12">
+				<div class="row">
+					<div class="col s12">
+
+						<h1 class="black-text" style="text-align: left;">The Double</h1>
+						<br>
+
+						<div class="hide-on-large-only" style="min-height:20vh;background: url(./assets/img/MVIMG_20171107_162943.jpg); right:0; background-position: center; background-size: 100%; "></div>
+
+
+						<br>
+						<h3 class="black-text" style="text-align: left;">Bedrooms: <span><b>2</b></span></h3>
+						<h3 class="black-text" style="text-align: left;">Bathrooms: <span><b>1</b></span></h3>
+						<h3 class="black-text" style="text-align: left;">Price: <span><b>$2350/mo</b></span></h3>
+					</div>
+				</div>
+				<br>
+				<br>
+				<div class="col s12 hide-on-med-and-down" style="border:1px solid black;">
+					<div class="row">
+						<div class="col s6 valign">
+							<div>
+								<h1 class="black-text" style="text-align:center;">2</h1>
+								<h3 class="black-text">Bed</h3>
+							</div>
+						</div>
+						<div class="col s6 valign">
+							<div>
+								<h1 class="black-text" style="text-align:center;">1</h1>
+								<h3 class="black-text">Bath</h3>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+<hr class="long">
+<section>
+	<div class="row">
+		<div class="container large">
+			<div class="col l6 s12">
+				<div class="row">
+					<div class="col s12">
+
+						<h1 class="black-text" style="text-align: right;">The Townhouse</h1>
+						<br>
+
+						<div class="hide-on-large-only" style="min-height:20vh;background: url(./assets/img/MVIMG_20171107_162943.jpg); right:0; background-position: center; background-size: 100%; "></div>
+
+
+						<br>
+						<h3 class="black-text" style="text-align: right;">Bedrooms: <span><b>2</b></span></h3>
+						<h3 class="black-text" style="text-align: right;">Bathrooms: <span><b>1</b></span></h3>
+						<h3 class="black-text" style="text-align: right;">Floor: <span><b>2nd</b></span></h3>
+						<h3 class="black-text" style="text-align: right;">Price: <span><b>$2495/mo</b></span></h3>
+					</div>
+				</div>
+				<br>
+				<br>
+				<div class="col s12 hide-on-med-and-down" style="border:1px solid black;">
+					<div class="row">
+						<div class="col s6 valign">
+							<div>
+								<h1 class="black-text" style="text-align:center;">2</h1>
+								<h3 class="black-text">Bed</h3>
+							</div>
+						</div>
+						<div class="col s6 valign">
+							<div>
+								<h1 class="black-text" style="text-align:center;">1</h1>
+								<h3 class="black-text">Bath</h3>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="col l1 hide-on-med-and-down"></div>
+			<div class="col l5 s12 hide-on-med-and-down">
+				<div style="min-height:90vh;background: url(./assets/img/MVIMG_20171107_162943.jpg); background-position: center; background-size: auto 100%; right:0;"></div>
 			</div>
 		</div>
 	</div>
@@ -120,61 +206,31 @@ include("./assets/template/home/topNav.php");
 				<br>
 				<div class="row"></div>
 				<div class="col s12 m6">
-
-					<p class="black-text">Washer</p><br>
-					<p class="black-text">Dryer</p><br>
-
-					<p class="black-text">Kitchen</p><br>
-
-					<p class="black-text">Family/kid friendly</p><br>
-
-					<p class="black-text">Closet</p>
-
-					<br>
-					<p class="black-text">Wood Floor</p>
-					<br>
-
-					<p class="black-text">Gated Entrance</p>
-					<br>
-
-					<p class="black-text">Private Parking</p>
-					<br>
-
-					<p class="black-text">Security Cameras</p>
-					<br>
-					<p class="black-text">Water</p><br>
-
-					<p class="black-text">Sewage</p>
+					<h5>Apartment Features</h5>
+					<p class="black-text">
+						Thoughtfully designed and spacious floor plans<br>
+						Short term leases available<br>
+						WiFi compatible<br>
+						Private patios<br>
+						Fully-equipped kitchens<br>
+						Separate dining areas<br>
+						Washer / Dryer hookup*
+					</p>
 				</div>
 				<br>
 				<div class="col s12 m6">
-
-					<p class="black-text">Washer</p><br>
-					<p class="black-text">Dryer</p><br>
-
-					<p class="black-text">Kitchen</p><br>
-
-					<p class="black-text">Family/kid friendly</p><br>
-
-					<p class="black-text">Closet</p>
-
-					<br>
-					<p class="black-text">Wood Floor</p>
-					<br>
-
-					<p class="black-text">Gated Entrance</p>
-					<br>
-
-					<p class="black-text">Private Parking</p>
-					<br>
-
-					<p class="black-text">Security Cameras</p>
-					<br>
-					<p class="black-text">Water</p><br>
-
-					<p class="black-text">Sewage</p>
+					<h5>Community Features</h5>
+					<p class="black-text">Sparkling swimming pool<br>
+						Pool-side BBQ area<br>
+						Assigned parking<br>
+						Sustainable lighting techniques to reduce energy consumption<br>
+						Friendly customer focused management team dedicated to taking care of your apartment needs<br>
+						Located near public transportation<br>
+						Easy access to Freeways 880 and 680<br>
+						Close to BART station
+					</p>
 				</div>
-
+<p>In seelect units</p>
 			</div>
 		</div>
 		<div class="col s12 l5">
@@ -195,18 +251,9 @@ include("./assets/template/home/topNav.php");
 
 		<div class="animation-element fade-in theNeighborhood">
 			<div style="padding:10%;">
-				<!--DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 				<h1 class="white-text">The <br>Neighborhood</h1>
 				<br>
-				<p class="white-text">Quincentennial California loft in the heart of everything! Spacious bright two bedroom, one bath with open kitchen, giant skylights and huge private roof. Steps to the city's best resturaurnts and nightlife. </p>
+				<p class="white-text">Fremont is a city in California's San Francisco Bay Area. The Niles Essanay Silent Film Museum traces the early days of filmmaking through artifacts and photos. Mission San José is a 1700s Spanish Mission, with a reconstructed church and a museum. The Ardenwood Historic Farm demonstrates horse-powered farming, and features a Victorian mansion. Central Park is home to Lake Elizabeth and the Aqua Adventure Water Park.</p>
 			</div>
 		</div>
 	</div>
@@ -246,12 +293,12 @@ DO SOMETHIGN WITH THIS LATER ~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 <!--FOURTH THING-->
 <section style="background-color:#CC4F55; position:relative;" class="large valign">
 	<div>
-		<h1>Enjoy Your Stay!</h1>
+		<h1 class="white-text">Enjoy Your Stay!</h1>
 	</div>
 	<div style="position:absolute; bottom:10%; left:10%;">
-		<p>Phone&nbsp;&nbsp;&nbsp;(510)-555-9196</p>
+		<p class="white-text">Phone&nbsp;&nbsp;&nbsp;<a href="tel:+15555551212">(510)-656-9196</a></p>
 		<br>
-		<p>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;email@email.com</p>
+		<p class="white-text">Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:email@email.com">email@email.com</a></p>
 		<br>
 		<p>Hours&nbsp;&nbsp;&nbsp;&nbsp;Monday - Friday : 1pm - 8pm</p>
 	</div>
